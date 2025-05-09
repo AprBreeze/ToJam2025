02c47f33f515cea709e4f9dc2342ff3b4e45d159
AprBreeze <immarkzhou@gmail.com>
2025-05-09 15:44:28 -0400

    NewProject

---
+++ ToJam/Source/TorontoJam/TorontoJamGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TorontoJamGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATorontoJamGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATorontoJamGameMode();
+};
+
+
+
